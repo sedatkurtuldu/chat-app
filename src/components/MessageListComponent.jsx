68ea84e1c5956b006116c85ac982bd45ce00b66b
Sedat Kurtuldu <sdt_krtldu@hotmail.com>
2024-08-04 15:42:23 +0300

    Kullanıcı listesine en son gönderilen mesaj ve zamanı getirildi.

--- src/components/MessageListComponent.jsx
+++ src/components/MessageListComponent.jsx
@@ -1,10 +1,11 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import moment from "moment";
 
 const MessageListComponent = ({ item, navigation }) => {
-    const navigateToChatRoom = () => {
-        navigation.navigate("ChatRoomScreen", { displayName: item.displayName, profileImage: item.profileImage, id: item.userId })
-    }
+  const navigateToChatRoom = () => {
+    navigation.navigate("ChatRoomScreen", { displayName: item.displayName, profileImage: item.profileImage, id: item.userId })
+  }
   return (
     <TouchableOpacity onPress={navigateToChatRoom} activeOpacity={0.8} className="flex-row justify-between items-center p-3 border-b border-gray-200">
       <View className="flex-row">
@@ -18,11 +19,15 @@ const MessageListComponent = ({ item, navigation }) => {
         </View>
         <View className="justify-center ml-4">
           <Text className="text-lg font-bold">{item.displayName}</Text>
-          <Text className="text-gray-600">Mesaj</Text>
+          {item.lastMessage && (
+            <Text className="text-gray-600">{item.lastMessage.Message}</Text>
+          )}
         </View>
       </View>
       <View className="items-end justify-center">
-        <Text className="text-gray-400">Saat</Text>
+        {item.lastMessage && (
+          <Text className="text-gray-400">{moment(item.lastMessage.SendTime).format("HH:mm")}</Text>
+        )}
       </View>
     </TouchableOpacity>
   );
